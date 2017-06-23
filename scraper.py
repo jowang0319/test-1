c9842cbeedf1b0f41c6a2c6fe3c289e0eb77ba1b
Zhizhou Wang <jowang0319@gmail.com>
2017-06-23 19:01:42 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,40 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
+import csv
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
+html = scraperwiki.scrape("http://www.boc.cn/sourcedb/whpj/index.html")
+
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+root = lxml.html.fromstring(html)
+root.cssselect("table")
+print(root.cssselect("table"))
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+my_table = root.cssselect("table")[1]
+
+list = []
+
+for tr in my_table.cssselect("tr"):
+    tds = tr.cssselect("td")
+#     print(len(tds))
+    if len(tds) == 8:
+        data_set = {
+            "货币名称":tds[0].text_content(),
+            "现汇买入价":tds[1].text_content(),
+            "现钞买入价":tds[2].text_content(),
+            "现汇卖出价":tds[3].text_content(),
+            "现钞卖出价":tds[4].text_content(),
+            "中行折算价":tds[5].text_content(),
+            "发布日期":tds[6].text_content(),
+            "发布时间":tds[7].text_content(),
+        }
+#         print(data_set)
+        list.append(data_set)
+print(list)
+
+with open('test.csv', 'w') as csvfile:
+    fieldnames = ["货币名称", "现汇买入价","现钞买入价","现汇卖出价","现钞卖出价","中行折算价","发布日期","发布时间"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for row in list:
+        writer.writerow(row)

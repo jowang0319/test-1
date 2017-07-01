80d9467c8daca00b365f24976deb150ef7c9775f
Zhizhou Wang <jowang0319@gmail.com>
2017-06-30 20:19:27 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,6 +16,7 @@ root.cssselect("table")
 print(root.cssselect("table"))
 
 my_table = root.cssselect("table")[1]
+page_url = "http://www.boc.cn/sourcedb/whpj/index.html"
 
 list = []
 
@@ -32,18 +33,44 @@ for tr in my_table.cssselect("tr"):
             "中行折算价":tds[5].text_content(),
             "发布日期":tds[6].text_content(),
             "发布时间":tds[7].text_content(),
+            "链接":page_url
         }
 #         print(data_set)
         list.append(data_set)
 print(list)
 
+pre_url = "http://www.boc.cn/sourcedb/whpj/index_"
+post_url = ".html"
+
+for i in range (1,11):
+    page_url = pre_url + str(i) + post_url
+    html = scraperwiki.scrape(page_url)
+    
+    root = lxml.html.fromstring(html)
+    root.cssselect("table")
+       
+    my_table = root.cssselect("table")[1]
+
+    for tr in my_table.cssselect("tr"):
+        tds = tr.cssselect("td")
+    #     print(len(tds))
+        if len(tds) == 8:
+            data_set = {
+                "货币名称":tds[0].text_content(),
+                "现汇买入价":tds[1].text_content(),
+                "现钞买入价":tds[2].text_content(),
+                "现汇卖出价":tds[3].text_content(),
+                "现钞卖出价":tds[4].text_content(),
+                "中行折算价":tds[5].text_content(),
+                "发布日期":tds[6].text_content(),
+                "发布时间":tds[7].text_content(),
+                "链接":page_url
+            }
+    #         print(data_set)
+            list.append(data_set)
+    
+#     print(page_url)
+
 table = scraperwiki.sqlite.save(unique_keys=['货币名称'], data=list)
 
 
-with open('test.csv', 'w') as csvfile:
-    fieldnames = ["货币名称", "现汇买入价","现钞买入价","现汇卖出价","现钞卖出价","中行折算价","发布日期","发布时间"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for row in list:
-        writer.writerow(row)
